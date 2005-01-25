76aadba0bd82c68f9b4985812236c30d78302d8b
mcnicks <mcnicks@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2005-01-25 21:37:04 +0000

    Tidied up comments and descriptions of functions.
    git-svn-id: https://plugins.svn.wordpress.org/flickr-post/trunk@584 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- flickr-post.php
+++ flickr-post.php
@@ -38,7 +38,7 @@ include_once( ABSPATH . "wp-content/plugins/flickr-post.conf" );
 
 
 
-/*** Constants - do not change any of these. ***/
+/*** Constants ***/
 
 
 
@@ -65,17 +65,19 @@ define( 'FP_DEBUG', 0 );
 
 
 
-/*** Functions - do not change any of these either. ***/
+/*** Functions ***/
 
 
 
-/* fp_add_photos
-    - $content contains the existing content of the post
-    - returns the existing content prefixed with photos
-
-   This is the filter that adds the photos to the top of the content
-   of the post. The call to the add_action function tells WordPress
-   to call the function last (since a priority of '0' is specified). */
+/*
+ * fp_add_photos
+ *  - $content contains the existing content of the post
+ *  - returns the existing content prefixed with photos
+ *
+ * This is the filter that adds the photos to the top of the content
+ * of the post. The call to the add_action function tells WordPress
+ * to call the function last (since a priority of '0' is specified).
+ */
 
 add_action( 'the_content', 'fp_add_photos', 0 );
 add_action( 'the_excerpt', 'fp_add_photos', 0 );
@@ -119,9 +121,10 @@ function fp_add_photos ( $content ) {
 
 
 
-/* fp_get_recent
-    - $number is the number of recent photos to return
-    - returns HTML containing thumbnail images of recent photos
+/*
+ * fp_get_recent
+ *  - $number is the number of recent photos to return
+ *  - returns HTML containing thumbnail images of recent photos
  */
 
 function fp_get_recent ( $number = 3 ) {
@@ -170,14 +173,18 @@ function fp_get_recent ( $number = 3 ) {
   return $fp_photos;
 }
 
-/* fp_url_cache
-    - $url a remote URL to cache locally
-    - returns a locally cached URL or $url
-  
-   This function checks to see whether my url_cache function has been
-   defined. If it has, then it will attempt to cache the given URL and
-   a local URL pointing to a cached copy of the remote file. If not,
-   the remote URL is returned unchanged. */
+
+
+/*
+ * fp_url_cache
+ *  - $url a remote URL to cache locally
+ *  - returns a locally cached URL or $url
+ *
+ * This function checks to see whether my url_cache function has been
+ * defined. If it has, then it will attempt to cache the given URL and
+ * a local URL pointing to a cached copy of the remote file. If not,
+ * the remote URL is returned unchanged.
+ */
 
 function fp_url_cache ( $url = "" ) {
 
@@ -217,16 +224,18 @@ function fp_get_user_id () {
 
 
 
-/* fp_get_photos
-    - $user_id is the ID of the user whose Flickr album we are displaying
-    - $slug is the slug of the current post
-    - returns two values in an array: an array of photo titles and an array
-      of partial photo URIs
-
-   This function returns the front section of the URI for each photo
-   that matches the given user_id and slug. The partial URI can be suffixed
-   with the appropriate ending to refer to the actual thumbnail or full
-   photo. */
+/*
+ * fp_get_photos
+ *  - $user_id is the ID of the user whose Flickr album we are displaying
+ *  - $slug is the slug of the current post
+ *  - returns two values in an array: an array of photo titles and an array
+ *    of partial photo URIs
+ *
+ * This function returns the front section of the URI for each photo
+ * that matches the given user_id and slug. The partial URI can be suffixed
+ * with the appropriate ending to refer to the actual thumbnail or full
+ * photo.
+ */
 
 function fp_get_photos ( $user_id, $slug ) {
   global $id;
@@ -275,18 +284,20 @@ function fp_get_photos ( $user_id, $slug ) {
 
 
 
-/* fp_rest_request
-    - $method is the REST method which should be called
-    - $args specified the arguments that should be sent
-    - (optional) $slug a slug used to cache results locally
-    - (optional) $timeout an optional timeout period for the cache
-    - returns an XML document containing the response
-
-   This function issues a REST request based on the specified method
-   and arguments. It adds mandatory REST arguments to those specified,
-   converts the REST response it receives into an XML document and
-   returns it. If a cache slug is given, the last request matching
-   the method and slug will be returned if it is fresh enough. */
+/*
+ * fp_rest_request
+ *  - $method is the REST method which should be called
+ *  - $args specified the arguments that should be sent
+ *  - (optional) $slug a slug used to cache results locally
+ *  - (optional) $timeout an optional timeout period for the cache
+ *  - returns an XML document containing the response
+ *
+ * This function issues a REST request based on the specified method
+ * and arguments. It adds mandatory REST arguments to those specified,
+ * converts the REST response it receives into an XML document and
+ * returns it. If a cache slug is given, the last request matching
+ * the method and slug will be returned if it is fresh enough.
+ */
 
 function fp_rest_request ( $method = "", $args = "", $slug = "", $timeout = "" ) {
 
@@ -339,17 +350,19 @@ function fp_rest_request ( $method = "", $args = "", $slug = "", $timeout = "" )
 
 
 
-/* fp_get_cached_response
-    - $method is the REST method to be cached
-    - $slug is used to make the cached response more unique
-    - (optional) $timeout is a specific timeout value to use
-    - (optional) $any will return any available cached
-      response, regardless of how stale it is.
-
-   This function uses $method and $cache to make up a unique filename
-   which is used to store REST responses. If the file associated with
-   $request and $slug exists, its contents are returned. If the file
-   is stale, then nothing is returned, which should prompt a refresh. */
+/* 
+ * fp_get_cached_response
+ *  - $method is the REST method to be cached
+ *  - $slug is used to make the cached response more unique
+ *  - (optional) $timeout is a specific timeout value to use
+ *  - (optional) $any will return any available cached
+ *    response, regardless of how stale it is.
+ *
+ * This function uses $method and $cache to make up a unique filename
+ * which is used to store REST responses. If the file associated with
+ * $request and $slug exists, its contents are returned. If the file
+ * is stale, then nothing is returned, which should prompt a refresh.
+ */
 
 function fp_get_cached_response( $method, $slug, $timeout = 0, $any = 0 ) {
 
@@ -396,14 +409,16 @@ function fp_get_cached_response( $method, $slug, $timeout = 0, $any = 0 ) {
 
 
 
-/* fp_cache_response
-    - $method is the REST method to be cached
-    - $slug is used to make the cached response more unique
-    - $response is the actual response that we should cache
-
-   This function uses $method and $cache to make up a unique filename
-   which is used to store REST responses. When called, the function
-   writes the given response to that filename. */
+/*
+ * fp_cache_response
+ *  - $method is the REST method to be cached
+ *  - $slug is used to make the cached response more unique
+ *  - $response is the actual response that we should cache
+ *
+ * This function uses $method and $cache to make up a unique filename
+ * which is used to store REST responses. When called, the function
+ * writes the given response to that filename.
+ */
 
 function fp_cache_response( $method, $slug, $response ) {
 
@@ -430,13 +445,14 @@ function fp_cache_response( $method, $slug, $response ) {
 
 
 
-/* fp_cache_value
-    - $name the name of the value to cache
-    - $value the value itself
-
-   This function uses the standard caching functions above to cache
-   name/value pairs. At the moment this is a bit of a fudge, making
-   use of the special method, "value". */
+/*
+ * fp_cache_value
+ *  - $name the name of the value to cache
+ *  - $value the value itself
+ *
+ * This function uses the standard caching functions above to cache
+ * name/value pairs. At the moment this is a bit of a fudge, making
+ * use of the special method, "value". */
 
 function fp_cache_value( $name, $value ) {
   return fp_cache_response( "value", $name, $value );
@@ -444,14 +460,16 @@ function fp_cache_value( $name, $value ) {
 
 
 
-/* fp_get_cached_value
-    - $name the name of the value to cache
-    - returns the cached value associated with $name
-
-   This function uses the standard caching functions above to return
-   a previously cached value associated with a name/value pair. At the
-   moment this is a bit of a fudge, making use of the special method,
-   "value". */
+/*
+ * fp_get_cached_value
+ *  - $name the name of the value to cache
+ *  - returns the cached value associated with $name
+ *
+ * This function uses the standard caching functions above to return
+ * a previously cached value associated with a name/value pair. At the
+ * moment this is a bit of a fudge, making use of the special method,
+ * "value".
+ */
 
 function fp_get_cached_value( $name ) {
   return fp_get_cached_response( "value", $name );
@@ -459,18 +477,18 @@ function fp_get_cached_value( $name ) {
 
 
 
-/* fp_make_xml_tree
-    - $xml is a string containing XML code
-    - returns a multi-dimensional data structure containing the XML
-
-   Note: I borrowed this code from Ramon Darrow's Flickr Gallery plugin,
-   which is available at:
-
-     http://www.worrad.com/archives/2004/11/30/flickr-gallery-wp-plugin/
-
-   This function takes a given string which contains XML and parses
-   it, returning a structure of associative arrays that contain the
-   various XML tags, attributes, values and content. */
+/*
+ * fp_make_xml_tree
+ *  - $xml is a string containing XML code
+ *  - returns a multi-dimensional data structure containing the XML
+ *
+ * Note: I borrowed this code from Ramon Darrow's Flickr Gallery plugin,
+ * which is available at:
+ *   http://www.worrad.com/archives/2004/11/30/flickr-gallery-wp-plugin/
+ * This function takes a given string which contains XML and parses
+ * it, returning a structure of associative arrays that contain the
+ * various XML tags, attributes, values and content.
+ */
 
 function fp_make_xml_tree( $xml ) {
 
@@ -500,12 +518,13 @@ function fp_make_xml_tree( $xml ) {
 
 
 
-/* get_the_slug
-    - returns the slug associated with the current post
-
-   This is borrowed from the code for get_the_title in:
-     wp-includes/template-functions-post.php
-*/
+/*
+ * get_the_slug
+ *  - returns the slug associated with the current post
+ *
+ * This is borrowed from the code for get_the_title in:
+ *   wp-includes/template-functions-post.php
+ */
 
 function get_the_slug () {
   global $wp_version, $id, $wpdb, $tableposts;
