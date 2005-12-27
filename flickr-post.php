146dbc13ea002d5e4114b70afd04d06686a7345b
mcnicks <mcnicks@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2005-12-27 20:19:13 +0000

    Removed path from the include_once() call that includes the flickr-post.conf file.
    git-svn-id: https://plugins.svn.wordpress.org/flickr-post/trunk@4276 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- flickr-post.php
+++ flickr-post.php
@@ -34,7 +34,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 /* Load the config file. */
 
-include_once( ABSPATH . "wp-content/plugins/flickr-post.conf" );
+include_once( "flickr-post.conf" );
 
 
 
